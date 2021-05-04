bad3db33c6e55060edac258225afd79e5647c00a
Leo O'Donnell <leopold.odonnell@gmail.com>
2021-05-04 16:16:58 -0400

    Added new relic instrumentation

---
+++ test-runner/src/init-newrelic.ts
@@ -0,0 +1,18 @@
+/**
+ * NewRelic EventStore instrumentation.
+ * Adds datastore metrics to the appendToStream, deleteStream, readStream, readAll client calls.
+ * More info @doc http://newrelic.github.io/node-newrelic/docs/tutorial-Datastore-Simple.html
+ */
+ function instrumentEventStoreWithNewRelic(shim: any, eventstore: any) {
+    shim.setDatastore('EventStore')
+    const proto = eventstore.EventStoreDBClient.prototype
+    shim.recordOperation(proto, ['appendToStream', 'deleteStream', 'readStream', 'readAll'], {
+      callback: shim.LAST,
+      promise: true,
+    })
+  }
+
+  if (process.env.NEW_RELIC_ENABLED == 'true') {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require('newrelic').instrumentDatastore('@eventstore/db-client', instrumentEventStoreWithNewRelic)
+  }
